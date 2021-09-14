299e412cbe31a3c2629f825a13155306b33f6b05
Albert Minnie <albert.minnie@gmail.com>
2021-09-14 14:19:40 +0200

    refactor to move implementation to its own function and add skeletons for other functions

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/lovecalc", (req, res) => {
+// Constants used for aggregating scores
+const CLASSIC_TOTAL = 100.0;
+const OCEAN_TOTAL = 200.0;
+const DIMORPHIC_TOTAL = 6.0;
+const CLASSIC_WEIGHT = 0.2;
+const OCEAN_WEIGHT = 0.5;
+const DIMORPHIC_WEIGHT = 0.3;
+
+function classicCalc(person1, person2) {
   // Convert to the fully expanded string (parameters are lower-cased and spaces are not added on purpose)
-  const loveString =
-    req.query.person1.toLowerCase() + "loves" + req.query.person2.toLowerCase();
+  const loveString = person1.toLowerCase() + "loves" + person2.toLowerCase();
 
   // Build a set of letters so we can strike out (remove) any letters when we iterate through string
   let stringSet = new Set(loveString);
@@ -15,7 +22,7 @@ app.get("/lovecalc", (req, res) => {
   for (let i = 0; i < loveString.length; i++) {
     const theChar = loveString[i];
     if (stringSet.has(theChar)) {
-      // Count number of occurences of the character
+      // Count number of occurences of the character and add to array
       occurences.push(
         (loveString.match(new RegExp(theChar, "g")) || []).length
       );
@@ -44,8 +51,43 @@ app.get("/lovecalc", (req, res) => {
     reductionArray = tempArray;
   }
 
-  // Convert final score to integer
-  const score = parseInt(reductionArray.join(""));
+  // Convert final score to float
+  const score = parseFloat(reductionArray.join(""));
+
+  return score;
+}
+
+function oceanCalc(person1, person2) {
+  // Note: Hardcoded to 100 as I cannot lookup scores (in database for instance) for the given person
+  return 100.0;
+}
+
+function dimorphicCalc(person1, person2) {
+  // Hardcoded to 0
+  return 0.0;
+}
+
+// Aggregate function. Normalizes scores so it is in a range between 0 and 1 and applies hardcoded weights, then returns the final
+// weighted score
+function aggregate(classic, ocean, dimorphic) {
+  const classicNormalized = classic / CLASSIC_TOTAL;
+  const oceanNormalized = ocean / OCEAN_TOTAL;
+  const dimorphicNormalized = dimorphic / DIMORPHIC_TOTAL;
+
+  const finalScore =
+    classicNormalized * CLASSIC_WEIGHT +
+    oceanNormalized * OCEAN_WEIGHT +
+    dimorphicNormalized * DIMORPHIC_WEIGHT;
+
+  return finalScore;
+}
+
+app.get("/lovecalc", (req, res) => {
+  const classic = classicCalc(req.query.person1, req.query.person2);
+  const ocean = oceanCalc(req.query.person1, req.query.person2);
+  const dimorphic = dimorphicCalc(req.query.person1, req.query.person2);
+
+  const score = aggregate(classic, ocean, dimorphic);
 
   res.json({ score: score });
 });
